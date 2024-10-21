3870c1c27175b404771fa94bede1ed26d540618b
Matheus Duarte <84824460+plmdsmatheus@users.noreply.github.com>
2024-10-21 18:39:49 +0000

    Fix: fixing tests v3

--- users/tests.py
+++ users/tests.py
@@ -5,33 +5,32 @@ from follows.models import Follow
 from rest_framework.test import APITestCase
 from rest_framework import status
 from users.models import CustomUser
+from django.core.exceptions import ValidationError
 
 
 class UserModelTest(TestCase):
 
-    def setUp(self):
-        self.user = CustomUser.objects.create_user(username="testuser", password="12345", email="user1@example.com")
-        self.user2 = CustomUser.objects.create_user(username="testuser2", password="1pop5", email="user2@exemple.com")
+    def test_username_length_limit(self):
+        user = CustomUser(username='a' * 21, email='user@example.com')
+        with self.assertRaises(ValidationError):
+            user.full_clean()
 
-    def test_user_creation(self):
-        self.assertEqual(CustomUser.objects.count(), 2)
+    def test_password_complexity(self):
+        user = CustomUser(username='user1', password='simple', email='user1@example.com')
+        with self.assertRaises(ValidationError): 
+            user.full_clean() 
 
-    def test_user_data_create(self):
-        self.assertEqual(self.user.username, "testuser")
-        self.assertEqual(self.user2.username, "testuser2")
-
-    def test_user_follow(self):
-        follow = Follow.objects.create(follower=self.user, following=self.user2)
-        self.assertEqual(Follow.objects.filter(following=self.user2).count(), 1)
-        self.assertEqual(follow.follower, self.user)
-        self.assertEqual(follow.following, self.user2)
-
+        user.set_password('Complex123!') 
+        try:
+            user.full_clean() 
+        except ValidationError: 
+            self.fail("") # This line should not be reached
 
 class UserAPITest(APITestCase):
 
     def test_register_user(self):
         url = "/users/register/"
-        data = {"username": "new_user", "password": "password123", "email": "newuser@example.com"}
+        data = {"username": "new_user", "password": "Password123!", "email": "newuser@example.com"}
         response = self.client.post(url, data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(CustomUser.objects.count(), 1)
